684e1be920132f8e2e1977cc1ac0f02db675729a
Julio J. Ticona Quispe <julioticona1508@gmail.com>
2021-07-05 08:44:38 -0500

    Adding 2 prefabs
    border and corner prefabs added

---
+++ app/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(":D");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("XD");
+        float vertical = Input.GetAxis("Vertical");
+        transform.position = transform.position + vertical * transform.forward * 10f * Time.deltaTime;
+
+        float velocidadGiro = 55f * Input.GetAxis("Horizontal");
+        transform.Rotate(0f, velocidadGiro * Time.deltaTime, 0f);
+    }
+}
